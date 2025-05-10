f3f01a3930d56f1788cb7a830c2b3febe4a4ed82
Josué Obed Landaverde <129899297+jlndvr@users.noreply.github.com>
2025-05-10 11:05:18 -0500

    Create DataHolder.hpp

---
+++ DataHolder.hpp
@@ -0,0 +1,57 @@
+using namespace std;
+
+template<typename T>
+class DataHolder {
+  private:
+  T storedValue;
+  T maxValue;
+  T minValue;
+
+  public:
+  DataHolder(T val, T min, T max) : minValue(min), maxValue(max) {
+    if (val > min && val < max ) {
+      storedValue = val;
+    }
+    else if (val > max) {
+      storedValue = max;
+      cout << "WARNING: " << val << " is too high! Setting value to " << max << "!" << endl;
+    }
+    else if (val < min) {
+      storedValue = min;
+      cout << "WARNING: " << val << " is too low! Setting " << val << " to " << min << endl;
+    }
+  }
+
+void setValue(T newValue) {
+    if (storedValue < maxValue && storedValue > minValue) {
+      newValue = storedValue;
+    }
+    else if (newValue > maxValue) {
+      maxValue = storedValue;
+      cout << "WARNING: val is too high! Setting value to max!" << endl;
+    }
+    else if (newValue < minValue) {
+      minValue = storedValue;
+      cout << "WARNING: val is too low! Setting value to min!" << endl;
+    }
+  }
+
+T getValue() const{
+  return storedValue;
+}
+};
+
+template<> 
+class DataHolder<std::string> {
+  private:
+  string storedValue;
+
+  public:
+  DataHolder(string val) : storedValue(val) {}
+  void setValue(string newString) {
+    storedValue = newString;
+  }
+  string getValue() {
+    return storedValue;
+  }
+};
