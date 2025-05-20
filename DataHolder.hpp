e9fa4ff056afac557d3989589b8d7fb9ff31a4c1
Josué Landaverde <129899297+jlndvr@users.noreply.github.com>
2025-05-20 03:06:33 +0000

    First commit

--- DataHolder.hpp
+++ DataHolder.hpp
@@ -1,57 +1,81 @@
-using namespace std;
+#ifndef DATAHOLDER_HPP
+#define DATAHOLDER_HPP
+
+#include <iostream>
+#include <string>
 
 template<typename T>
+
 class DataHolder {
+
   private:
+
   T storedValue;
   T maxValue;
   T minValue;
 
   public:
-  DataHolder(T val, T min, T max) : minValue(min), maxValue(max) {
+
+  DataHolder (T val, T min, T max) : minValue(min), maxValue(max) {
+
     if (val > min && val < max ) {
       storedValue = val;
     }
+
     else if (val > max) {
       storedValue = max;
       cout << "WARNING: " << val << " is too high! Setting value to " << max << "!" << endl;
     }
+
     else if (val < min) {
       storedValue = min;
       cout << "WARNING: " << val << " is too low! Setting " << val << " to " << min << endl;
     }
+
   }
 
-void setValue(T newValue) {
+void setValue (T newValue) {
+
     if (storedValue < maxValue && storedValue > minValue) {
       newValue = storedValue;
     }
+
     else if (newValue > maxValue) {
       maxValue = storedValue;
       cout << "WARNING: val is too high! Setting value to max!" << endl;
     }
+
     else if (newValue < minValue) {
       minValue = storedValue;
       cout << "WARNING: val is too low! Setting value to min!" << endl;
     }
-  }
 
-T getValue() const{
+  }
+ 
+T getValue () const {
   return storedValue;
 }
+
 };
 
 template<> 
-class DataHolder<std::string> {
+
+class DataHolder<string> {
+
   private:
+
   string storedValue;
 
   public:
-  DataHolder(string val) : storedValue(val) {}
-  void setValue(string newString) {
+
+  DataHolder (string val) : storedValue(val) {}
+
+  void setValue (string newString) {
     storedValue = newString;
   }
-  string getValue() {
-    return storedValue;
-  }
+
+  string getValue () { return storedValue; }
+  
 };
+
+#endif
--- functions.hpp
+++ functions.hpp
@@ -1,17 +1,21 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
 #include <iostream>
-#include <string>
 #include <algorithm>
 
 using namespace std;
 
 template<typename T>
-T midpoint(T a, T b) {
+T midpoint (T a, T b) {
   T m = a + ((b - a) / 2);
   return m;
 } 
 
 template<typename T>
-void swapValues(T& a, T& b) {
-  swap(a, b);
+void swapValues (T &a, T &b) {
+  swap (a, b);
   cout << "A: " << a << " B: " << b << endl;
 }
+
+#endif
